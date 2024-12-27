804def175de1a49337d0a5b53d038711a0e5c8c3
Aldosimone <aldosimone@live.com>
2024-12-27 16:46:14 +0100

    first commit

---
+++ poolingaround/src/main/java/com/poolingaround/App.java
@@ -0,0 +1,13 @@
+package com.poolingaround;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
